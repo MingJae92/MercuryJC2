ed7f6998b41c92d4aba6fcb6db9bb4ed943ad400
Ming Chi <wong_mingchi@yahoo.co.uk>
2022-07-01 17:33:57 +0100

    My work image grid system implemnted

--- src/routes/MyWork.js
+++ src/routes/MyWork.js
@@ -1,14 +1,39 @@
 import React from 'react'
 import './MyWork.css'
 import ArtWorkImages from '../ArtWorkImages'
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 const MyWork = () => {
+  
   return (
     <div>
-      <h1>My Work</h1>
-      ArtWorkImages.
+     <div>
+          <Box >
+            <Grid container spacing={1}>
+              {ArtWorkImages.map(
+                (item)=>(
+                  <Grid item xs={4}>
+                    <Item><img src={item.path}/></Item>
+                  </Grid>
+                  )
+              )}
+            </Grid>
+            </Box>
+      </div>
     </div>
   )
 }
 
 export default MyWork
+
