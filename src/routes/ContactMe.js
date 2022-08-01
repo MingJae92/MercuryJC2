96201364b3308a870b2a8531c338745cdf4dd887
Ming Chi <wong_mingchi@yahoo.co.uk>
2022-08-01 18:14:52 +0100

    Contact form implemented

--- src/routes/ContactMe.js
+++ src/routes/ContactMe.js
@@ -1,10 +1,53 @@
 import React from 'react'
 import "./ContactMe.css"
+import Grid from '@mui/material/Grid';
+import { TextField, Button, Card, CardContent, Typography } from '@mui/material';
+
+
+
 
 const ContactMe = () => {
   return (
     <div>
       <h1>Contact Me</h1>
+      <Typography gutterBottom variant="h3" align="center">
+       
+       </Typography>
+      <Grid>
+        <Card style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto" }}>
+          <CardContent>
+            <Typography gutterBottom variant="h5">
+              Contact Us
+          </Typography> 
+            <Typography variant="body2" color="textSecondary" component="p" gutterBottom>
+            <p>Fill up the form and our team will get back to you within 24 hours.</p>  
+          </Typography> 
+            <form>
+              <Grid container spacing={1}>
+                <Grid xs={12} sm={6} item>
+                  <TextField placeholder="Enter first name" label="First Name" variant="outlined" fullWidth required />
+                </Grid>
+                <Grid xs={12} sm={6} item>
+                  <TextField placeholder="Enter last name" label="Last Name" variant="outlined" fullWidth required />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField type="email" placeholder="Enter email" label="Email" variant="outlined" fullWidth required />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField type="number" placeholder="Enter phone number" label="Phone" variant="outlined" fullWidth required />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField label="Message" multiline rows={4} placeholder="Type your message here" variant="outlined" fullWidth required />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button type="submit" variant="contained" color="primary" fullWidth>Submit</Button>
+                </Grid>
+
+              </Grid>
+            </form>
+          </CardContent>
+        </Card>
+      </Grid>
     </div>
   )
 }
