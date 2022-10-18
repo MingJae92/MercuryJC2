0c965ff5b79bb798ae4d333397e458dd5d1a1c07
Ming Chi <wong_mingchi@yahoo.co.uk>
2022-10-18 05:28:12 +0100

    Pusher object updated again

--- src/server/server.js
+++ src/server/server.js
@@ -29,7 +29,7 @@ app.listen(port, ()=>{
 })
 
 const pusher = new Pusher({
-    appId: process.env.PUSHER_APP_ID,
+      appId: process.env.PUSHER_APP_ID,
       key: process.env.PUSHER_APP_KEY,
       secret: process.env.PUSHER_APP_SECRET,
       cluster: process.env.PUSHER_APP_CLUSTER,
