cd8de5da00e122da6a6a5aba09a9f8bc9111240a
Ming Chi <wong_mingchi@yahoo.co.uk>
2022-10-26 17:38:07 +0100

    axios implemented

--- src/routes/Comments.js
+++ src/routes/Comments.js
@@ -2,33 +2,35 @@ import { TextField, Button, Card, CardContent, Typography } from '@mui/material'
 import Grid from '@mui/material/Grid';
 import { useEffect, useState } from 'react';
 import "./Comments.css"
+import axios from 'axios';
 
 const Comments = () => {
-  // const [userName, setUserName]=useState("")
-  // const [comments, setComments]=useState("")
-  // const [newComments, setNewComments]=useState("")
+  const [userName, setUserName]=useState("")
+  const [comment, setComment]=useState([])
+  const [newComments, setNewComments]=useState("")
 
-  //  const updateInput = (e)=>{
-  //  const [name, value] = useState({
-  //   name:name.e.target,
-  //   value:value.e.target
-  //  })
-   
-  //  }
 
-  // const postComment = (e)=>{
-  //   e.preventDefault();
-  //   const {userName, newComments} = useState("")
-  //   if(userName.trim() || newComments.trim()==="") return;
-  //   const data ={
-  //     name:userName,
-  //     text: newComments,
-  //     votes: 0
-  //   };
-    
-  //   useEffect(()=>{
-  //     axios.post("/localhost/5000/comment", data).then(res.setNewComments(res.data))
-  //   },[])
+  const postComment = (e)=>{
+    e.preventDefault();
+
+    if(userName.trim()==="" || newComments.trim()==="") return;
+    const data ={
+      name:userName,
+      text: newComments,
+      votes: 0
+    };
+    axios.post("http://localhost:5000/comment", data)
+    console.log(data)
+      .then(()=>{
+        setUserName("")
+        setComment("")
+      })
+      .catch(error => console.log(error));
+  }
+    useEffect(()=>{
+
+      
+    },[])
 
   
   return (
@@ -40,16 +42,16 @@ const Comments = () => {
             <p>Feel free to make some comments!!!</p>  
           </Typography> 
             
-            <form>
+            <form onSubmit={postComment}>
               <Grid container spacing={1}>
                 <Grid xs={12} sm={6} item>
-                    <TextField placeholder="Enter first name" name="firstname" variant="outlined" style={{padding:"0.5em"}} fullWidth required />
-                  </Grid>
-                  <Grid xs={12} sm={6} item>
-                    <TextField placeholder="Enter last name" name="lastname" variant="outlined" style={{padding:"0.5em"}} fullWidth required />
+                    <TextField placeholder="Enter username" name="username" variant="outlined" value={userName} onChange={(e)=>{setUserName(e.target.value)}} fullWidth required />
                   </Grid>
                   <Grid item xs={12}>
-                    <TextField name="message" multiline rows={4} placeholder="Type your message here" variant="outlined" style={{padding:"0.5em"}} fullWidth required />
+                    <TextField name="comment" multiline rows={4} placeholder="Comment here" variant="outlined" value={comment} onChange={(e)=>{setComment(e.target.value)}} style={{padding:"0.5em"}} fullWidth required />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField name="newComment" multiline rows={4} placeholder="" variant="outlined" value={newComments} onChange={(e)=>{setNewComments(e.target.value)}} style={{padding:"0.5em"}} fullWidth required />
                   </Grid>
                   <Grid item xs={12}>
                     <Button style={{height: "5vh"}}  variant="contained" color="primary" fullWidth>Submit</Button>
