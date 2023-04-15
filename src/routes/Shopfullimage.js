7902bf3529cd8c7d2e9235737266062abaaa5004
Ming Chi <mingchi@Mings-MBP.broadband>
2023-04-15 02:51:15 +0100

    useffect hook updated on Shopfullimage component

--- src/routes/Shopfullimage.js
+++ src/routes/Shopfullimage.js
@@ -2,20 +2,20 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router"
 import axios from "axios"
 
-
-
 const Shopfullimage =()=>{
     const[ shopFullImage, setShopFullImage] = useState([])
 
     let {shop_item_url_path} = useParams()
     const url = `http:localhost:7000/Shop/:${shop_item_url_path}`
+    
     useEffect(()=>{
-        axios.get(url)
-    }).then(res=>{
-        const shopFullImageData = res.data
-        // setShopFullImage(shopFullImageData)
-        console.log(shopFullImageData)
-    }, [])
+        axios.get(url).then((res)=>{
+            const shopFullImageData = res.data;
+            console.log(shopFullImageData)
+            setShopFullImage(shopFullImageData)
+        })
+    }
+        )
     return(
         <div>
             {shopFullImage.map((item)=>(<div>{item.image_preview}{item.images_main}</div>))}
