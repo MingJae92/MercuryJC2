507f12bd4c87fa58ed118c596aa905e0174adbef
Ming Chi <mingchi@Mings-MBP.broadband>
2023-04-26 17:49:30 +0100

    Array of full images display implemented

--- src/routes/Shopfullimage.js
+++ src/routes/Shopfullimage.js
@@ -2,23 +2,29 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router"
 import axios from "axios"
 
-const Shopfullimage =()=>{
-    const[ shopFullImage, setShopFullImage] = useState([])
-    
-    let {shop_item_url_path} = useParams()
+const Shopfullimage = () => {
+    const [shopFullImage, setShopFullImage] = useState([])
+
+    let { shop_item_url_path } = useParams()
     const url = `http://localhost:7000/Shop/${shop_item_url_path}`
-    
-    useEffect(()=>{
-        axios.get(url).then((res)=>{
+
+    useEffect(() => {
+        axios.get(url).then((res) => {
             const shopFullImageData = res.data;
             console.log(shopFullImageData)
             setShopFullImage(shopFullImageData)
-        }).catch(error=>console.log(error))
+        }).catch(error => console.log(error))
     }
-        ,[])
-    return(
+        , [])
+    return (
         <div>
-            {shopFullImage.map((item)=>(<div key={item.id}><h1>{item.description}</h1><img src={item.images_main}/></div>))}
+            {shopFullImage.map((item) => (
+                <div key={item.id}>
+                    <h1>{item.description}</h1>
+                    {item.images_main.map((image_url) => (
+                        <img src={image_url}/>
+                    ))}
+                </div>))}
         </div>
     )
 }
