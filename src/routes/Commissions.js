1019d4bd54df777e6023fb0357f99a350f1a23f6
Ming Chi <mingchi@Mings-MBP.broadband>
2023-05-21 22:41:38 +0100

    Redudant code removed

--- src/routes/Commissions.js
+++ src/routes/Commissions.js
@@ -28,29 +28,6 @@ const Commissions = () => {
     <div className='illustrations'>
       <h1>Commissions</h1>
       <p>These are just previews only of my commissions.</p>
-      {/* <h1>ILLUSTRATION(FULL-COLOUR)</h1>
-        <h5>BUST</h5>
-          <img src={Pardofelis}/>
-          <img src={YaeMiko}/>
-        <h5>WAIST-UP</h5>
-          <img src={MonarchMona}/>
-          <img src={PoroKingxMatty}/>
-        <h5>FULL-BODY</h5>
-          <img src={PorcelainGanyu}/>
-          <img src={StarchasmHutao}/>
-      <h1>ILLUSTRATION(SIMPLE-COLOUR)</h1>
-        <h5>HEADSHOT</h5>
-          <img src={Headshot1}/>
-        <h5>WAIST-UP</h5>
-        <img src={Waistup1}/>
-      <h1>CHIBI</h1>
-        <h5>HEADSHOT</h5>
-          <img src={Headshot2}/>
-        <h5>FULL-BODY</h5>
-          <img src={Fullbody}/>
-      <h1>EMOTES</h1>
-        <h5>STREAM EMOTES</h5>
-        <img src={Emotes}/> */}
       {commissionsImages.map((item)=>(<img src={item.imageUrl} />))}
     <ScrollToTop/>
     <Footer/>
